27571c679b111d2d09b6fd8b8451f2cd847b612a
Matt Paul <mattpaul001@icloud.com>
2016-01-06 21:26:20 +0000

    Express set up

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    cons = require('consolidate'),
+    mongodb = require('mongodb');
+
+
+
+// var http = require('http');
+
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello, World");
+// });
+
+// server.listen(8000);
+
+// console.log("Server running on localhost 8000");
